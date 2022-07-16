742aa964453c2507960cb6c8b3a4c417c7fcaef5
mrudulkolambe <mrudulkolambe02@gmail.com>
2022-07-16 14:52:09 +0530

    Initial Commit

--- src/pages/admin/Card.js
+++ src/pages/admin/Card.js
@@ -4,10 +4,10 @@ const Card = ({ data }) => {
 	return (
 		<>
 			<div style={{ width: '100%' }}>
-				<div style={{ fontSize: '18px', width: '100%', fontWeight: 'bold' }}>Event: {data.eventName}</div>
-				<div style={{ fontSize: '18px', width: '100%' }}>Date: {data.eventDate}</div>
-				<div style={{ fontSize: '18px', width: '100%' }}>Speaker: {data.speaker}</div>
-				<div style={{ fontSize: '18px', width: '100%' }}>{data.registrations} registrations</div>
+				<div style={{ fontSize: '18px', width: '100%', fontWeight: 'bold' }}>Event: {data && data.eventName}</div>
+				<div style={{ fontSize: '18px', width: '100%' }}>Date: {data && data.eventDate}</div>
+				<div style={{ fontSize: '18px', width: '100%' }}>Speaker: {data && data.speaker}</div>
+				<div style={{ fontSize: '18px', width: '100%' }}>{data && data.registrations} registrations</div>
 			</div>
 		</>
 	)
